5e58795f926a2567bdffa6e48a3718fe75a94b72
Rikuto Yamaguchi <yamaguchirkt@gmail.com>
2021-02-26 01:57:58 +0900

    Add FetchNotificationsUsecase

--- src/container.ts
+++ src/container.ts
@@ -1,7 +1,12 @@
 import { container } from 'tsyringe';
 import { Fetch } from 'src/infrastructures/Fetch';
+import { NotifulApiRepository } from 'src/infrastructures/repositories/NotifulApiRepository';
 
 container.register('Fetch', {
   // TODO: update api endpoint
   useValue: new Fetch('api'),
+});
+
+container.register('NotifulApiRepository', {
+  useClass: NotifulApiRepository,
 });
